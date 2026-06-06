e615fc9b7c37faf8145ce9890743fa3dc0debfba
satishucm-jpg <satishucm@gmail.com>
2026-06-06 15:54:56 -0500

    Use DATABASE_URL environment variable

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,11 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 
-DATABASE_URL = (
+load_dotenv()
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
     "mysql+pymysql://asksql_user:asksql_pass@localhost:3306/asksql_db"
 )
 
