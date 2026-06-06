aca5e0fb776f9a99223959fd8476874bb04fd941
satishucm-jpg <satishucm@gmail.com>
2026-06-06 16:32:28 -0500

    Connect Streamlit frontend to deployed backend

--- frontend/app.py
+++ frontend/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import pandas as pd
 
-API_URL = "http://127.0.0.1:8000/ask"
+API_URL = "API_URL = "https://asksql-ai.onrender.com/ask""
 
 st.set_page_config(page_title="AskSQL AI", page_icon="🧠", layout="wide")
 
